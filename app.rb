193ca7807577d026d00f7541e55590bf157707f4
Shinichiro Aska <s.aska.org@gmail.com>
2013-03-03 18:24:54 +0900

    add hello world.

---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'rubygems' unless defined? ::Gem
+require 'sinatra'
+get '/' do
+  'Hello world!'
+end
---
+++ script/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/var/lib/gems/1.8/bin/unicorn /home/aska/7kai-Bootstrap/config.ru -E production -p 10001
